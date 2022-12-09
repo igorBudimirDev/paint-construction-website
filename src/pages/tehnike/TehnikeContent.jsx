fbf089f7bc0df9e710f937f44f00c0f159231fdf
Igor Budimir <igor.budimir.developer@gmail.com>
2022-12-09 14:17:37 +0100

    14.3

--- src/pages/tehnike/TehnikeContent.jsx
+++ src/pages/tehnike/TehnikeContent.jsx
@@ -15,7 +15,7 @@ const TehnikeContent = () => {
         <div>
           <img src={BetonImg} alt="" className="pb-5" />
         </div>
-        <p className="text-white text-xl font-light">
+        <p className="text-white text-2xl font-light">
           Beton efekat je jako popularna dekorativna <br />
           tehnika današnjice.Pomoću palete svih <br />
           nijansi možete stvoriti moderan izgled
@@ -28,7 +28,7 @@ const TehnikeContent = () => {
           <h2 className="text-6xl pb-10 text-white font-light">SAHARA</h2>
           <img src={SaharaImg} alt="" />
         </div>
-        <p className="text-white text-xl font-light">
+        <p className="text-white text-2xl font-light">
           Sahara je namenjena za <br />
           zaštitu i dekoraciju <br />
           unutrašnjih zidnih <br />
@@ -41,12 +41,13 @@ const TehnikeContent = () => {
           Maxidecor Sahara.
         </p>
       </div>
+
       <div className="tehnike__content__3 pb-10 flex flex-col">
         <h2 className="text-6xl pb-10 text-white font-light">ISTINTO</h2>
         <div>
           <img src={IstintoImg} alt="" className="pb-5" />
         </div>
-        <p className="text-white text-xl font-light">
+        <p className="text-white text-2xl font-light">
           Dekorativni materijal Istinto predstavlja jedinstvo <br />
           između prirode, arhitekture i dizajna, a svaki zid <br />
           pretvara u unikatno umetničko delo. Ovaj proizvod na <br />
@@ -54,6 +55,52 @@ const TehnikeContent = () => {
           dozvoljava vam da na zidu stvorite reljefnu ili poliranu <br />
           površinu i na taj način u samo jednom premazu <br />
           pokrijete manje nepravilnosti zida.
+        </p>
+      </div>
+
+      <div className="tehnike__content__3 flex w-[1022px] flex-row-reverse justify-between items-center">
+        <div className="tehnike_-content__3--imgdiv">
+          <h2 className="text-6xl pb-10 text-white font-light">CALCE CRUDA</h2>
+          <img src={CalceImg} alt="" />
+        </div>
+        <p className="text-white text-2xl font-light">
+          Calce cruda je porozan <br />
+          dekorativni premaz sa veoma <br />
+          prirodnim osećajem.Formulisan <br />
+          je kombinacijom hidratisanog <br />
+          kreča i prečišćene gline, što daje <br />
+          efekat rezbarenja.
+        </p>
+      </div>
+
+      <div className="tehnike__content__4 flex w-[1022px] justify-between items-center">
+        <div className="tehnike_-content__4--imgdiv">
+          <h2 className="text-6xl pb-10 text-white font-light">OCEANO</h2>
+          <img src={OceanoIng} alt="" />
+        </div>
+        <p className="text-white text-2xl font-light">
+          Tehnika oceano nam pruža <br />
+          osećaj plutanja morskim <br />
+          talasima, što je čini jednom od <br />
+          ikona dekorativnih tehnika.
+        </p>
+      </div>
+
+      <div className="tehnike__content__5 flex w-[1022px] flex-row-reverse justify-between items-center">
+        <div className="tehnike_-content__5--imgdiv">
+          <h2 className="text-6xl pb-10 text-white font-light">OTTOCENTO</h2>
+          <img src={OttocentoImg} alt="" />
+        </div>
+        <p className="text-white text-2xl font-light">
+          Ottocento je boja na vodenoj <br />
+          bazi, za zidove za unutrašnju <br />
+          dekorativnu upotrebu.Posebno <br />
+          je osmišljena kako bi se prostoru <br />
+          dala visoko prefinjena <br />
+          elegancija.Ottocento <br />
+          je napravljen najsavremenijom <br />
+          tehnologijom i garantuje <br />
+          trajnost.
         </p>
       </div>
     </div>
