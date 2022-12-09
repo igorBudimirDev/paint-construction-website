76d4442c3b8273f87f57e50c93cefa21c9305d51
Igor Budimir <igor.budimir.developer@gmail.com>
2022-12-09 17:32:26 +0100

    commit 15.3

--- src/pages/home/HomeHero.jsx
+++ src/pages/home/HomeHero.jsx
@@ -5,6 +5,10 @@ const HomeHero = () => {
     console.warn(uslugeItem[0]);
     uslugeItem[0].scrollIntoView();
   };
+  const scrollToKontakt = () => {
+    const kontaktItem = document.getElementsByClassName("kontakt")
+    kontaktItem[0].scrollIntoView();
+  }
   return (
     <div className="hero bg-[url('./assets/hero-background.png')]">
       <div className="hero__container h-screen flex justify-center items-center bg-overlay-color">
@@ -19,6 +23,7 @@ const HomeHero = () => {
           </div>
           <div className="hero__container__content--buttons flex gap-6 md:flex-row md:gap-6">
             <button className="w-[120px] px-3 py-1 mx-1 border-solid border-white border-2 text-[20px] text-white hover:bg-white-overlay-color hover:text-black"
+            onClick={scrollToKontakt}
             >
               Kontakt
             </button>
--- src/pages/home/HomeUsluge.jsx
+++ src/pages/home/HomeUsluge.jsx
@@ -3,25 +3,25 @@ const Home_usluge = () => {
     <div className="usluge h-screen bg-main-green flex justify-center items-center ">
       <div className="usluge__wrapper h-[70%] w-[80%] bg-[url('./assets/usluge-background.png')] bg-cover">
         <div className="usluge__wrapper__overlay h-[100%] w-[100%] bg-dark-white-overlay-color flex flex-col">
-            <h3 className="pt-10 pb-20 text-center text-2xl font-light md:text-2xl md:text-left md:pl-[20px] lg:text-4xl mobile:text-xl mobile:pb-5">Usluge koje Vam nudimo</h3>
-            <div className="cene-content h-[100%] flex flex-col items-center text-center ld:text-left ld:px-[20px] lg:flex-row mobile:text-[12px] mobile:justify-around">
-              <div className="cene-content-main w-[60%] h-[100%] flex flex-col justify-around items-center text-center md:text-left">
-              <span className="w-[100%] h-[] font-bold md:border-b md:border-b-underline-color lg:text-xl mobile:px-[10px]">GLETOVANJE</span> <br />
-                <span className="w-[100%] h-[] font-bold md:border-b md:border-b-underline-color lg:text-xl mobile:px-[10px]">KREČENJE</span> <br />
-                <span className="w-[100%] h-[] font-bold md:border-b md:border-b-underline-color lg:text-xl mobile:px-[10px]">IZRADA DEKORATIVNIH RADOVA SA DOMAĆIM I STRANIM MATERIJALIMA</span> <br />
-                <span className="w-[100%] h-[] font-bold md:border-b md:border-b-underline-color lg:text-xl mobile:px-[10px]">GLETOVANJE POD RASVETOM</span> <br />
-                <span className="w-[100%] h-[] font-bold md:border-b md:border-b-underline-color lg:text-xl mobile:px-[10px]">KREČENJE AKRILNIM BOJAMA OIKOS, VITEX, TIKURILA, MAXIMA, CASSATI</span> <br />
-                <span className="w-[100%] h-[] font-bold md:border-b md:border-b-underline-color lg:text-xl mobile:px-[10px]">ZAŠTITA PROZORA PRED POČETAK RADOVA</span> <br />
-                <span className="w-[100%] h-[] font-bold md:border-b md:border-b-underline-color lg:text-xl mobile:px-[10px]">POPRAVKA TAPETA</span> <br />
-                <span className="w-[100%] h-[] font-bold md:border-b md:border-b-underline-color lg:text-xl mobile:px-[10px]">KRPLJENJE, ISPRAVLJANJE ZIDOVA MAŠINSKIM MALTERIMA</span> <br />
+            <h3 className="pt-5 pb-p text-center text-xl font-light md:text-2xl md:text-left md:pl-[20px] lg:text-4xl mobile:text-xl mobile:pb-5">Usluge koje Vam nudimo</h3>
+            <div className="cene-content h-[100%] flex flex-col items-center text-center ld:text-left lg:flex-row ">
+              <div className="cene-content-main w-[100%] h-[100%] flex flex-col justify-around md:maxw-[60%] items-center text-center md:text-left">
+              <span className="w-[100%] md:px-10 font-bold md:border-b md:border-b-underline-color lg:text-xl ">GLETOVANJE</span> <br />
+                <span className="w-[100%] md:px-10 font-bold md:border-b text-sm md:border-b-underline-color lg:text-xl">KREČENJE</span> <br />
+                <span className="w-[100%] md:px-10 font-bold md:border-b text-sm md:border-b-underline-color lg:text-xl">IZRADA DEKORATIVNIH RADOVA SA DOMAĆIM I STRANIM MATERIJALIMA</span> <br />
+                <span className="w-[100%] md:px-10 font-bold md:border-b text-sm md:border-b-underline-color lg:text-xl">GLETOVANJE POD RASVETOM</span> <br />
+                <span className="w-[100%] md:px-10 font-bold md:border-b text-sm md:border-b-underline-color lg:text-xl">KREČENJE AKRILNIM BOJAMA OIKOS, VITEX, TIKURILA, MAXIMA, CASSATI</span> <br />
+                <span className="w-[100%] md:px-10 font-bold md:border-b text-sm md:border-b-underline-color lg:text-xl">ZAŠTITA PROZORA PRED POČETAK RADOVA</span> <br />
+                <span className="w-[100%] md:px-10 font-bold md:border-b text-sm md:border-b-underline-color lg:text-xl">POPRAVKA TAPETA</span> <br />
+                <span className="w-[100%] md:px-10 font-bold md:border-b text-sm md:border-b-underline-color lg:text-xl">KRPLJENJE, ISPRAVLJANJE ZIDOVA MAŠINSKIM MALTERIMA</span> <br />
               </div>
-                <div className="cene-content-add h-[100%] flex flex-col items-center justify-center">
+                <div className="cene-content-add h-[100%] flex flex-col items-center justify-start md:justify-center">
                 <h3 className="pt-10 text-center text-xl font-light md:text-xl md:text-left md:pl-[20px] mobile:text-xl mobile:pb-5">Organizacija građevinskih radova</h3>
                 <div className="cene-content-add-main flex flex-col">
-                <span className="w-[100%] h-[] font-bold text-center lg:text-xl mobile:px-[10px]">ELEKTROINSTALACIJE</span> <br />
-                <span className="w-[100%] h-[] font-bold text-center lg:text-xl mobile:px-[10px]">GIPSANI RADOVI</span> <br />
-                <span className="w-[100%] h-[] font-bold text-center lg:text-xl mobile:px-[10px]">HIDROINSTALACIJE</span> <br />
-                <span className="w-[100%] h-[] font-bold text-center lg:text-xl mobile:px-[10px]">KERAMIKA</span> <br />
+                <span className="w-[100%] font-bold text-center lg:text-xl mobile:px-[10px]">ELEKTROINSTALACIJE</span> <br />
+                <span className="w-[100%] font-bold text-center lg:text-xl mobile:px-[10px]">GIPSANI RADOVI</span> <br />
+                <span className="w-[100%] font-bold text-center lg:text-xl mobile:px-[10px]">HIDROINSTALACIJE</span> <br />
+                <span className="w-[100%] font-bold text-center lg:text-xl mobile:px-[10px]">KERAMIKA</span> <br />
                 </div>
                 </div>
             </div>
