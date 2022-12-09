d899461230fabd3526391a2f87452118105d64c2
Igor Budimir <igor.budimir.developer@gmail.com>
2022-12-09 19:42:23 +0100

    commit 16.5

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -36,7 +36,7 @@ const Footer = () => {
         <div className="footer__content__right flex items-center md:h-[80%] md:items-end">
           <div className="footer__content__right-icons w-[140px] flex justify-center pb-1 border-b-white border-b gap-2 md:h-[13%]">
             <a target="blank" href="/">
-              <img src={CustomPhone} alt="" />
+              <img href="tel:+381632748948" src={CustomPhone} alt="" />
             </a>
             <a
               target="blank"
