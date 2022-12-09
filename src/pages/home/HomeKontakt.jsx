ca3770b61c64680ba2727e5328f0e02e233266da
Igor Budimir <igor.budimir.developer@gmail.com>
2022-12-09 19:56:52 +0100

    commit 17

--- src/pages/home/HomeKontakt.jsx
+++ src/pages/home/HomeKontakt.jsx
@@ -10,17 +10,21 @@ const Home_kontakt = () => {
             Kontakt informacije
           </h3>
           <div className="kontakt__content h-[100%] flex flex-col items-center text-center md:px-[20px] justify-around">
-            <div className="kontakt__content--phone flex flex-col items-center md:flex-row">
-              <BsTelephoneFill className=" text-4xl" />
-              <span className="text-[1em] sm:text-2xl mt-2 md:mt-0 md:ml-5 md:text-4xl font-light">
-                063/274-89-48
-              </span>
+            <div className="kontakt__content--phone border-2 rounded-lg p-2 flex flex-col items-center md:flex-row">
+              <a href="tel:0632748948">
+                <BsTelephoneFill className=" text-4xl" />
+              </a>
+              <a href="tel:0632748948">
+                <span className="text-[1em] sm:text-2xl mt-2 md:mt-0 md:ml-5 md:text-4xl font-light">
+                  063/274-89-48
+                </span>
+              </a>
             </div>
-            <div className="kontakt__content--phone flex flex-col items-center md:flex-row">
-              <AiFillInstagram className=" text-4xl" />
-              <span className="text-[1em] sm:text-2xl mt-2 md:mt-0 md:ml-5 md:text-4xl font-light">
+            <div className="kontakt__content--phone border-2 rounded-lg p-2 flex flex-col items-center md:flex-row">
+              <a href="https://www.instagram.com/molerski_radovi_milenko_kostic/"> <AiFillInstagram className=" text-4xl" /></a>
+             <a href="https://www.instagram.com/molerski_radovi_milenko_kostic/"> <span className="text-[1em] sm:text-2xl mt-2 md:mt-0 md:ml-5 md:text-4xl font-light">
                 @molerski.radovi.kostic
-              </span>
+              </span> </a>
             </div>
             <div className="kontakt__content--phone flex flex-col items-center md:flex-row">
               <MdEmail className=" text-4xl" />
