7986904364cb2ffeca9832311526ca95eb60dced
Igor Budimir <igor.budimir.developer@gmail.com>
2022-12-09 08:10:43 +0100

    commit 14.2

--- src/pages/gallery/GalleryImages.jsx
+++ src/pages/gallery/GalleryImages.jsx
@@ -47,7 +47,97 @@ import Done46 from "../../assets/gallery-assets/done 46.jpg";
 import Done48 from "../../assets/gallery-assets/done 48.jpg";
 import Done49 from "../../assets/gallery-assets/done 49.jpg";
 import Done50 from "../../assets/gallery-assets/done 50.jpg";
-import Done70 from "../../assets/gallery-assets/done 70.jpg";
+import Done51 from "../../assets/gallery-assets/done 51.jpg";
+import Done52 from "../../assets/gallery-assets/done 52.jpg";
+import Done53 from "../../assets/gallery-assets/done 53.jpg";
+import Done54 from "../../assets/gallery-assets/done 54.jpg";
+import Done55 from "../../assets/gallery-assets/done 55.jpg";
+import Done56 from "../../assets/gallery-assets/done 56.jpg";
+import Done57 from "../../assets/gallery-assets/done 57.jpg";
+import Done58 from "../../assets/gallery-assets/done 58.jpg";
+import Done59 from "../../assets/gallery-assets/done 59.jpg";
+import Done60 from "../../assets/gallery-assets/done 60.jpg";
+import Done61 from "../../assets/gallery-assets/done 61.jpg";
+import Done62 from "../../assets/gallery-assets/done 62.jpg";
+import Done63 from "../../assets/gallery-assets/done 63.jpg";
+import Done64 from "../../assets/gallery-assets/done 64.jpg";
+import Done65 from "../../assets/gallery-assets/done 65.jpg";
+import Done66 from "../../assets/gallery-assets/done 66.jpg";
+import Done67 from "../../assets/gallery-assets/done 67.jpg";
+import Done68 from "../../assets/gallery-assets/done 68.jpg";
+import Done69 from "../../assets/gallery-assets/done 69.jpg";
+import Done80 from "../../assets/gallery-assets/done 80.jpg";
+import Done81 from "../../assets/gallery-assets/done 81.jpg";
+import Done82 from "../../assets/gallery-assets/done 82.jpg";
+import Done83 from "../../assets/gallery-assets/done 83.jpg";
+import Done84 from "../../assets/gallery-assets/done 84.jpg";
+import Done85 from "../../assets/gallery-assets/done 85.jpg";
+import Done86 from "../../assets/gallery-assets/done 86.jpg";
+import Done87 from "../../assets/gallery-assets/done 87.jpg";
+import Done88 from "../../assets/gallery-assets/done 88.jpg";
+import Done89 from "../../assets/gallery-assets/done 89.jpg";
+import Done90 from "../../assets/gallery-assets/done 90.jpg";
+import Done91 from "../../assets/gallery-assets/done 91.jpg";
+import Done92 from "../../assets/gallery-assets/done 92.jpg";
+import Done93 from "../../assets/gallery-assets/done 93.jpg";
+import Done94 from "../../assets/gallery-assets/done 94.jpg";
+import Done95 from "../../assets/gallery-assets/done 95.jpg";
+import Done96 from "../../assets/gallery-assets/done 96.jpg";
+import Done97 from "../../assets/gallery-assets/done 97.jpg";
+import Done98 from "../../assets/gallery-assets/done 98.jpg";
+import Done99 from "../../assets/gallery-assets/done 99.jpg";
+import Done100 from "../../assets/gallery-assets/done 100.jpg";
+import Done101 from "../../assets/gallery-assets/done 101.jpg";
+import Done102 from "../../assets/gallery-assets/done 102.jpg";
+import Done103 from "../../assets/gallery-assets/done 103.jpg";
+import Done104 from "../../assets/gallery-assets/done 104.jpg";
+import Done105 from "../../assets/gallery-assets/done 105.jpg";
+import Done106 from "../../assets/gallery-assets/done 106.jpg";
+import Done107 from "../../assets/gallery-assets/done 107.jpg";
+import Done108 from "../../assets/gallery-assets/done 108.jpg";
+import Done109 from "../../assets/gallery-assets/done 109.jpg";
+import Done110 from "../../assets/gallery-assets/done 110.jpg";
+import Done111 from "../../assets/gallery-assets/done 111.jpg";
+import Done112 from "../../assets/gallery-assets/done 112.jpg";
+import Done113 from "../../assets/gallery-assets/done 113.jpg";
+import Done114 from "../../assets/gallery-assets/done 114.jpg";
+import Done115 from "../../assets/gallery-assets/done 115.jpg";
+import Done116 from "../../assets/gallery-assets/done 116.jpg";
+import Done117 from "../../assets/gallery-assets/done 117.jpg";
+import Done118 from "../../assets/gallery-assets/done 118.jpg";
+import Done119 from "../../assets/gallery-assets/done 119.jpg";
+import Done120 from "../../assets/gallery-assets/done 120.jpg";
+import Done121 from "../../assets/gallery-assets/done 121.jpg";
+import Done122 from "../../assets/gallery-assets/done 122.jpg";
+import Done123 from "../../assets/gallery-assets/done 123.jpg";
+import Done124 from "../../assets/gallery-assets/done 124.jpg";
+import Done125 from "../../assets/gallery-assets/done 125.jpg";
+import Done126 from "../../assets/gallery-assets/done 126.jpg";
+import Done127 from "../../assets/gallery-assets/done 127.jpg";
+import Done128 from "../../assets/gallery-assets/done 128.jpg";
+import Done129 from "../../assets/gallery-assets/done 129.jpg";
+import Done130 from "../../assets/gallery-assets/done 130.jpg";
+import Done131 from "../../assets/gallery-assets/done 131.jpg";
+import Done132 from "../../assets/gallery-assets/done 132.jpg";
+import Done133 from "../../assets/gallery-assets/done 133.jpg";
+import Done134 from "../../assets/gallery-assets/done 134.jpg";
+import Done135 from "../../assets/gallery-assets/done 135.jpg";
+import Done136 from "../../assets/gallery-assets/done 136.jpg";
+import Done137 from "../../assets/gallery-assets/done 137.jpg";
+import Done138 from "../../assets/gallery-assets/done 138.jpg";
+import Done139 from "../../assets/gallery-assets/done 139.jpg";
+import Done140 from "../../assets/gallery-assets/done 140.jpg";
+import Done141 from "../../assets/gallery-assets/done 141.jpg";
+import Done142 from "../../assets/gallery-assets/done 142.jpg";
+import Done143 from "../../assets/gallery-assets/done 143.jpg";
+import Done144 from "../../assets/gallery-assets/done 144.jpg";
+import Done145 from "../../assets/gallery-assets/done 145.jpg";
+import Done146 from "../../assets/gallery-assets/done 146.jpg";
+import Done147 from "../../assets/gallery-assets/done 147.jpg";
+import Done148 from "../../assets/gallery-assets/done 148.jpg";
+import Done149 from "../../assets/gallery-assets/done 149.jpg";
+import Done150 from "../../assets/gallery-assets/done 150.jpg";
+
 import { AiOutlineClose } from "react-icons/ai";
 const GalleryImages = () => {
   let data = [
@@ -96,9 +186,98 @@ const GalleryImages = () => {
     { id: 45, imgSrc: Done45 },
     { id: 46, imgSrc: Done46 },
     { id: 48, imgSrc: Done48 },
-    { id: 39, imgSrc: Done49 },
+    { id: 49, imgSrc: Done49 },
     { id: 50, imgSrc: Done50 },
-    { id: 70, imgSrc: Done70 },
+    { id: 51, imgSrc: Done51 },
+    { id: 52, imgSrc: Done52 },
+    { id: 53, imgSrc: Done53 },
+    { id: 54, imgSrc: Done54 },
+    { id: 55, imgSrc: Done55 },
+    { id: 56, imgSrc: Done56 },
+    { id: 57, imgSrc: Done57 },
+    { id: 58, imgSrc: Done58 },
+    { id: 59, imgSrc: Done59 },
+    { id: 60, imgSrc: Done60 },
+    { id: 61, imgSrc: Done61 },
+    { id: 62, imgSrc: Done62 },
+    { id: 63, imgSrc: Done63 },
+    { id: 64, imgSrc: Done64 },
+    { id: 65, imgSrc: Done65 },
+    { id: 66, imgSrc: Done66 },
+    { id: 67, imgSrc: Done67 },
+    { id: 68, imgSrc: Done68 },
+    { id: 69, imgSrc: Done69 },
+    { id: 80, imgSrc: Done80 },
+    { id: 81, imgSrc: Done81 },
+    { id: 82, imgSrc: Done82 },
+    { id: 83, imgSrc: Done83 },
+    { id: 84, imgSrc: Done84 },
+    { id: 85, imgSrc: Done85 },
+    { id: 86, imgSrc: Done86 },
+    { id: 87, imgSrc: Done87 },
+    { id: 88, imgSrc: Done88 },
+    { id: 89, imgSrc: Done89 },
+    { id: 90, imgSrc: Done90 },
+    { id: 91, imgSrc: Done91 },
+    { id: 92, imgSrc: Done92 },
+    { id: 93, imgSrc: Done93 },
+    { id: 94, imgSrc: Done94 },
+    { id: 95, imgSrc: Done95 },
+    { id: 96, imgSrc: Done96 },
+    { id: 97, imgSrc: Done97 },
+    { id: 98, imgSrc: Done98 },
+    { id: 99, imgSrc: Done99 },
+    { id: 100, imgSrc: Done100 },
+    { id: 101, imgSrc: Done101 },
+    { id: 102, imgSrc: Done102 },
+    { id: 103, imgSrc: Done103 },
+    { id: 104, imgSrc: Done104 },
+    { id: 105, imgSrc: Done105 },
+    { id: 106, imgSrc: Done106 },
+    { id: 107, imgSrc: Done107 },
+    { id: 108, imgSrc: Done108 },
+    { id: 109, imgSrc: Done109 },
+    { id: 110, imgSrc: Done110 },
+    { id: 111, imgSrc: Done111 },
+    { id: 112, imgSrc: Done112 },
+    { id: 113, imgSrc: Done113 },
+    { id: 114, imgSrc: Done114 },
+    { id: 115, imgSrc: Done115 },
+    { id: 116, imgSrc: Done116 },
+    { id: 117, imgSrc: Done117 },
+    { id: 118, imgSrc: Done118 },
+    { id: 119, imgSrc: Done119 },
+    { id: 120, imgSrc: Done120 },
+    { id: 121, imgSrc: Done121 },
+    { id: 122, imgSrc: Done122 },
+    { id: 123, imgSrc: Done123 },
+    { id: 124, imgSrc: Done124 },
+    { id: 125, imgSrc: Done125 },
+    { id: 126, imgSrc: Done126 },
+    { id: 127, imgSrc: Done127 },
+    { id: 128, imgSrc: Done128 },
+    { id: 129, imgSrc: Done129 },
+    { id: 130, imgSrc: Done130 },
+    { id: 131, imgSrc: Done131 },
+    { id: 132, imgSrc: Done132 },
+    { id: 133, imgSrc: Done133 },
+    { id: 134, imgSrc: Done134 },
+    { id: 135, imgSrc: Done135 },
+    { id: 136, imgSrc: Done136 },
+    { id: 137, imgSrc: Done137 },
+    { id: 138, imgSrc: Done138 },
+    { id: 139, imgSrc: Done139 },
+    { id: 140, imgSrc: Done140 },
+    { id: 141, imgSrc: Done141 },
+    { id: 142, imgSrc: Done142 },
+    { id: 143, imgSrc: Done143 },
+    { id: 144, imgSrc: Done144 },
+    { id: 145, imgSrc: Done145 },
+    { id: 146, imgSrc: Done146 },
+    { id: 147, imgSrc: Done147 },
+    { id: 148, imgSrc: Done148 },
+    { id: 149, imgSrc: Done149 },
+    { id: 150, imgSrc: Done150 },
   ];
   const [model, setModel] = useState(false);
   const [tempImgSrc, setTempImgSrc] = useState("");
